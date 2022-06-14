d29c01b97112c4fadb0a80fa4ff5861bd864d5f5
马军 <majun@xiwicloud.com>
2022-06-14 16:09:36 +0800

    fix: 更改bug

--- example/v2/components/user-rulerts.vue
+++ example/v2/components/user-rulerts.vue
@@ -36,9 +36,9 @@ import {
   onMounted,
   nextTick
 } from 'vue-demi'
-import SketchRule from '../../../lib/index.es.js' // 这里可以换成打包后的
-// import SketchRule from '../../../src/index' // 这里可以换成打包后的
-import '../../../lib/style.css'
+// import SketchRule from '../../../lib/index.es.js' // 这里可以换成打包后的
+import SketchRule from '../../../src/index' // 这里可以换成打包后的
+// import '../../../lib/style.css'
 const rectWidth = 600
 const rectHeight = 320
 export default defineComponent({
@@ -74,6 +74,7 @@ export default defineComponent({
       }
     })
     onMounted(() => {
+      alert('msg')
       // 滚动居中
       screensRef.value.scrollLeft =
         containerRef.value.getBoundingClientRect().width / 2 - 400
